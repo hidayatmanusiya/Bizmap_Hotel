eebdef4f97cd3b196cafba2f73902fcdd3a89b60
rajat singh <rajat.singh@bizmap.in>
2022-09-12 13:09:31 +0530

    [Added status master In Room Folio]

--- bizmap_hotel/public/js/room_folio_hms.js
+++ bizmap_hotel/public/js/room_folio_hms.js
@@ -196,32 +196,5 @@ frappe.ui.form.on('Sales Book Item',"sales_order",function(frm,cdt,cdn){
    // });
 //});
 
-frappe.listview_settings['Room Folio HMS'] = {
-    
-    get_indicator: function(doc){
-        const status_color = {
-                "Checked Out":"green",
-                "Pre-Check In":"gray",
-                "Checked In":"yellow"
-
-            };
-        
-         if(doc.status==="Checked In"){
-            return [__(doc.status),status_color[doc.status]]
-        }
-        if(doc.status==="Checked Out"){
-            
-            return [__(doc.status),status_color[doc.status]]
-        }
-        if(doc.status==="Pre-Check In"){
-            return [__(doc.status),status_color[doc.status]]
-        }
-       
-        if(doc.status==="Skipper"){
-            return [__(doc.status),status_color[doc.status]]
-        }
-        
-    }
-}
 
 
