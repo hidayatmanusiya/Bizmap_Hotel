75619048816807a2bf3fd8258813fba3dd0414d9
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-11-24 16:32:45 +0530

    feat: show fetch bookings button according to integration settings

--- bizmap_hotel/public/js/sales_order_list.js
+++ bizmap_hotel/public/js/sales_order_list.js
@@ -1,19 +1,33 @@
-frappe.listview_settings['Sales Order'] = {
-    onload: function (listview) {
-        // eZee Booking Fetch Button
-        listview.page.add_inner_button("Fetch Booking", function () {
-            frappe.call({
-                method: "bizmap_hotel.utill.booking.insertbooking",
-                args: {}
-            });
-        });
-        // STAHH Booking Fetch Button
-        listview.page.add_inner_button("STAAH Booking", function () {
-            frappe.call({
-                method: "bizmap_hotel.utill.booking_staah.insertbooking",
-                args: {}
-            });
-        });
-    },
+frappe.listview_settings["Sales Order"] = {
+	onload: function(listview) {
+		fetch_bookings(listview);
+	},
+	refresh: function(listview) {
+		fetch_bookings(listview);
+	},
 };
 
+function fetch_bookings(listview) {
+	// eZee Booking Fetch Button
+	frappe.db.get_single_value("Integration Settings", "ezee").then((val) => {
+		if (val) {
+			listview.page.add_inner_button("Fetch Booking", function() {
+				frappe.call({
+					method: "bizmap_hotel.utill.booking.insertbooking",
+					args: {},
+				});
+			});
+		}
+	});
+	// STAHH Booking Fetch Button
+	frappe.db.get_single_value("Integration Settings", "staah").then((val) => {
+		if (val) {
+			listview.page.add_inner_button("STAAH Booking", function() {
+				frappe.call({
+					method: "bizmap_hotel.utill.booking_staah.insertbooking",
+					args: {},
+				});
+			});
+		}
+	});
+}
