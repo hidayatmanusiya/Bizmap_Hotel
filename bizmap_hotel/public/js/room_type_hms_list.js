f3319336201d388040a394182f292f59d28a108b
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-11-24 16:40:36 +0530

    feat: show update rooms button on room type hms according to integration settings

--- bizmap_hotel/public/js/room_type_hms_list.js
+++ bizmap_hotel/public/js/room_type_hms_list.js
@@ -1,19 +1,34 @@
-frappe.listview_settings['Room Type HMS'] = {
-    onload: function (listview) {
-        // eZee Booking Fetch Button
-        listview.page.add_inner_button("Update eZee Room", function () {
-            frappe.call({
-                method: "bizmap_hotel.utill.room.update_room",
-                args: {}
-            });
-        });
-        // STAHH Booking Fetch Button
-        listview.page.add_inner_button("Update STAAH Room", function () {
-            frappe.call({
-                method: "bizmap_hotel.utill.room_staah.update_room",
-                args: {}
-            });
-        });
-    },
+frappe.listview_settings["Room Type HMS"] = {
+	onload: function(listview) {
+		update_channel_rooms(listview);
+	},
+	refresh: function(listview) {
+		update_channel_rooms(listview);
+	},
 };
 
+function update_channel_rooms(listview) {
+	// eZee Booking Fetch Button
+	frappe.db.get_single_value("Integration Settings", "ezee").then((val) => {
+		if (val) {
+			listview.page.add_inner_button("Update eZee Room", function() {
+				frappe.call({
+					method: "bizmap_hotel.utill.room.update_room",
+					args: {},
+				});
+			});
+		}
+	});
+
+	frappe.db.get_single_value("Integration Settings", "staah").then((val) => {
+		if (val) {
+			// STAHH Booking Fetch Button
+			listview.page.add_inner_button("Update STAAH Room", function() {
+				frappe.call({
+					method: "bizmap_hotel.utill.room_staah.update_room",
+					args: {},
+				});
+			});
+		}
+	});
+}
